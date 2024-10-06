c5d28e326652b2e0fb0f01ad954b87f7f1beaa65
veeru.mot <veerender.veeru2009@gmail.com>
2024-10-06 13:29:59 -0400

    linkdein jobs data code

---
+++ dash_app.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import plotly.graph_objs as go
+import pandas as pd
+from dash_bootstrap_templates import load_figure_template
+import psycopg2
+import plotly.express as px
+from flask import Flask
+import os
+
+
+server = Flask(__name__)
+
+load_figure_template("DARKLY")
+
+dash_app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY], server=server, url_base_pathname='/jobs-data/')
+
+colors = {
+    'background': '#111111',
+    'text': 'lightgrey'
+}
+
+
+
+dash_app.layout = html.Div(children=[
+    html.H1(children='Linkdein DevOps Job Data Analysis', style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+        ),
+    
+
+    # html.Div(children='''
+    #     .
+    # '''),
+
+    dcc.Interval(
+        id="interval-component", interval=3600 * 1000, n_intervals=0
+    ),
+
+    dcc.Graph(
+        id='line-chart'
+    ),
+
+    dcc.Graph(
+        id='line-chart1'
+    ),
+
+    dcc.Graph(
+        id='line-chart2'
+    )
+])
+@dash_app.callback(
+    Output('line-chart', 'figure'),
+    Input('interval-component', 'n_intervals')
+)
+def daily_data_postgres(Input):
+    DB_NAME = os.environ.get('DB_NAME')
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASS = os.environ.get('DB_PASS')
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_PORT = os.environ.get('DB_PORT')
+    conn = psycopg2.connect(database=DB_NAME,
+                        user=DB_USER,
+                        password=DB_PASS,
+                        host=DB_HOST,
+                        port=DB_PORT)
+    print("Database connected successfully")
+    cur = conn.cursor()
+    query = """
+                SELECT date, day_jobs
+                FROM public.time_data
+                ORDER BY DATE DESC
+                LIMIT 7 ;
+            """
+    cur.execute(query)
+    day_data = cur.fetchall()
+    df = pd.DataFrame(day_data, columns= ['date', 'day_jobs'])
+    # print(df)
+    day_data = {
+    'date': df['date'].tolist(),  # Convert 'date' column to list
+    'value': df['day_jobs'].tolist()  # Convert 'day_jobs' column to list and rename to 'value'
+    }
+    print(day_data)
+    cur.close()
+    conn.close()
+    fig = px.line(day_data, x='date', y='value', title= 'Data for daily posted jobs in Linkdein', markers=True, text='value')
+    fig.update_traces(textposition='top center')
+    fig.update_layout( 
+        plot_bgcolor=colors['background'],
+        paper_bgcolor=colors['background'],
+        font_color=colors['text']
+        )
+    return fig
+
+@dash_app.callback(
+    Output('line-chart1', 'figure'),
+    Input('interval-component', 'n_intervals')
+)
+def weekly_data_postgres(Input):
+    DB_NAME = os.environ.get('DB_NAME')
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASS = os.environ.get('DB_PASS')
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_PORT = os.environ.get('DB_PORT')
+    conn = psycopg2.connect(database=DB_NAME,
+                        user=DB_USER,
+                        password=DB_PASS,
+                        host=DB_HOST,
+                        port=DB_PORT)
+    print("Database connected successfully")
+    cur = conn.cursor()
+    query = """
+                select current_week,round(avg(week_jobs))
+                from time_data td 
+                group by current_week
+                ORDER BY current_week
+                LIMIT 4 ;
+            """
+    cur.execute(query)
+    week_data = cur.fetchall()
+    df = pd.DataFrame(week_data, columns= ['current_week', 'week_jobs'])
+    # print(df)
+    week_data = {
+    'week': df['current_week'].tolist(),  # Convert 'date' column to list
+    'value': df['week_jobs'].tolist()  # Convert 'day_jobs' column to list and rename to 'value'
+    }
+    print(week_data)
+    cur.close()
+    conn.close()
+    fig = px.line(week_data, x='week', y='value', title= 'Data for weekly posted jobs in Linkdein', markers=True, text='value')
+    fig.update_traces(textposition='top center')
+    fig.update_layout( 
+        plot_bgcolor=colors['background'],
+        paper_bgcolor=colors['background'],
+        font_color=colors['text']
+        )
+    return fig
+
+@dash_app.callback(
+    Output('line-chart2', 'figure'),
+    Input('interval-component', 'n_intervals')
+)
+def montly_data_postgres(Input):
+    DB_NAME = os.environ.get('DB_NAME')
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASS = os.environ.get('DB_PASS')
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_PORT = os.environ.get('DB_PORT')
+    conn = psycopg2.connect(database=DB_NAME,
+                        user=DB_USER,
+                        password=DB_PASS,
+                        host=DB_HOST,
+                        port=DB_PORT)
+    print("Database connected successfully")
+    cur = conn.cursor()
+    query = """
+                select date_part('month', date::date) as month,
+                round(avg(month_jobs))
+                from time_data 
+                group by date_part('month', date::date)
+                order by month;
+            """
+    cur.execute(query)
+    month_data = cur.fetchall()
+    df = pd.DataFrame(month_data, columns= ['month', 'month_jobs'])
+    # print(df)
+    month_data = {
+    'month': df['month'].tolist(),  # Convert 'date' column to list
+    'value': df['month_jobs'].tolist()  # Convert 'day_jobs' column to list and rename to 'value'
+    }
+    print(month_data)
+    cur.close()
+    conn.close()
+    fig = px.line(month_data, x='month', y='value', title= 'Data for Monthly posted jobs in Linkdein', markers=True, text='value')
+    fig.update_traces(textposition='top center')
+    fig.update_layout( 
+        plot_bgcolor=colors['background'],
+        paper_bgcolor=colors['background'],
+        font_color=colors['text']
+        )
+    return fig
+
+if __name__ == "__main__":
+    dash_app.run_server(debug=True, host="0.0.0.0")
+
+
---
+++ link.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import re
+from datetime import date
+import psycopg2
+from selenium.common.exceptions import WebDriverException
+import os
+
+
+# driver = webdriver.Chrome()
+
+
+
+#Global variables
+total = None
+month = None
+week = None
+day = None
+Today_date = date.today()
+
+
+def jobs_count(driver):
+    global total, month, week, day
+    max_retries = 5  # Maximum number of retries
+    retry_count = 0
+    while retry_count < max_retries: 
+        try: 
+            path = f"https://www.linkedin.com/jobs/search?keywords=DevOps%20Engineer&location=United%20States"
+            driver.get(path)
+            time.sleep(5)
+            driver.find_element(
+                By.XPATH, '//*[@id="base-contextual-sign-in-modal"]/div/section'
+            )
+            close_button = driver.find_element(
+                By.XPATH, '//*[@id="base-contextual-sign-in-modal"]/div/section/button'
+            )
+            close_button.click()
+# except NoSuchElementException:
+            posted_time = driver.find_element(
+                By.XPATH, '//*[@id="jserp-filters"]/ul/li[1]/div'
+            )
+            posted_time.click()
+            anytime = driver.find_element(
+                By.XPATH, '//*[@id="jserp-filters"]/ul/li[1]/div/div/div/div/div/div[1]'
+            )
+            total = anytime.text
+            total = re.sub(r"[^\d\s]", "", total)
+            print(total)
+            pastmonth = driver.find_element(
+                By.XPATH, '//*[@id="jserp-filters"]/ul/li[1]/div/div/div/div/div/div[2]'
+            )
+            month = pastmonth.text
+            month = re.sub(r"[^\d\s]", "", month)
+            print(month)
+            pastweek = driver.find_element(
+                By.XPATH, '//*[@id="jserp-filters"]/ul/li[1]/div/div/div/div/div/div[3]'
+            )
+            week = pastweek.text
+            week = re.sub(r"[^\d\s]", "", week)
+            print(week)
+            past24hrs = driver.find_element(
+                By.XPATH, '//*[@id="jserp-filters"]/ul/li[1]/div/div/div/div/div/div[4]'
+            )
+            day = past24hrs.text
+            day = re.sub(r"[^\d\s]", "", day)
+            day = day.replace("24", "", 1).strip()
+            print(day)
+        # with open("page.html", "a") as f:
+        # #     print(driver.page_source, file=f)
+            time.sleep(1)
+        # driver.get_screenshot_as_file(
+        #     "/Users/madhurigorantla/github/My-website/veeru-portfolio/user.png"
+        # )
+            break
+        except WebDriverException as e:
+        # Handle failure and retry
+                print(f"Failed to load URL on attempt {retry_count + 1}. Retrying...")
+        retry_count += 1
+        time.sleep(2)
+    if retry_count == max_retries:
+        print("Failed to load URL after multiple attempts")
+    driver.quit()
+
+
+
+def use_values():
+    global Today_date
+    print(f"month_jobs:{month}, week_jobs:{week}, day_jobs:{day}, all:{total}")
+    DB_NAME = os.environ.get('DB_NAME')
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASS = os.environ.get('DB_PASS')
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_PORT = os.environ.get('DB_PORT')
+    month_jobs = month
+    week_jobs = week
+    day_jobs = day
+    conn = psycopg2.connect(
+        database=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT
+    )
+    print("Database connected successfully")
+    cur = conn.cursor()
+    query = """
+                INSERT INTO time_data (date, month_jobs, week_jobs, day_jobs)
+                VALUES (%s, %s, %s, %s)
+            """
+    cur.execute(query, (Today_date, month_jobs, week_jobs, day_jobs))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+jobs_count(webdriver.Chrome())
+use_values()
