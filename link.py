9bc2ea60658a31bba0e9d0e3876e1183bfaa20b6
veerumot <38892436+veerumot@users.noreply.github.com>
2024-10-14 22:09:28 -0400

    Update link.py

--- link.py
+++ link.py
@@ -5,9 +5,9 @@ from selenium.webdriver.common.by import By
 import time
 import re
 from datetime import date
-import psycopg2
+# import psycopg2
 from selenium.common.exceptions import WebDriverException
-import os
+# import os
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import chromedriver_autoinstaller
@@ -117,28 +117,19 @@ def jobs_count(driver):
 def use_values():
     global Today_date
     print(f"month_jobs:{month}, week_jobs:{week}, day_jobs:{day}, all:{total}")
-    DB_NAME = os.environ.get('DB_NAME')
-    DB_USER = os.environ.get('DB_USER')
-    DB_PASS = os.environ.get('DB_PASS')
-    DB_HOST = os.environ.get('DB_HOST')
-    DB_PORT = os.environ.get('DB_PORT')
     month_jobs = month
     week_jobs = week
     day_jobs = day
-    conn = psycopg2.connect(
-        database=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT
-    )
-    print("Database connected successfully")
-    cur = conn.cursor()
-    query = """
-                INSERT INTO time_data (date, month_jobs, week_jobs, day_jobs)
-                VALUES (%s, %s, %s, %s)
-            """
-    cur.execute(query, (Today_date, month_jobs, week_jobs, day_jobs))
-
-    conn.commit()
-    cur.close()
-    conn.close()
+    data = [
+            ["month_jobs", "week_jobs", "day_jobs"],
+            [month_jobs, week_jobs, day_jobs]
+    ]
+    # print(data)
+    with open("data.csv", "w", newline="") as file: 
+        writer = csv.writer(file)
+        writer.writerow(data[0]) 
+        for row in data[1:]: 
+            writer.writerow(row)
 
 
 def main():
