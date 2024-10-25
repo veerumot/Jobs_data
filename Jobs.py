30a5a8657c95ca991a316b5f2fbefc1bd9d63923
veerumot <38892436+veerumot@users.noreply.github.com>
2024-10-25 09:10:48 -0400

    Update Jobs.py

--- Jobs.py
+++ Jobs.py
@@ -8,6 +8,37 @@ from datetime import date
 import datetime
 import csv
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))  
+display.start()
+
+chromedriver_autoinstaller.install()
+
+
+chrome_options = webdriver.ChromeOptions()
+options = [
+  # Define window size here
+   "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+ 
+    #"--headless",
+    #"--disable-gpu",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    #"--no-sandbox",
+    #"--disable-dev-shm-usage",
+    #'--remote-debugging-port=9222'
+]
+
+for option in options:
+    chrome_options.add_argument(option)
+
+    
+driver = webdriver.Chrome(options = chrome_options)
 
 
 # Global variables
